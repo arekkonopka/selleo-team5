d187cee10287f5bd116ca9e9ef04db497c9e712a
Japonics <japonics.email@gmail.com>
2021-10-22 17:59:05 +0200

    Change from class to function

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,23 @@ import {
     Route,
 } from 'react-router-dom';
 import React from 'react';
-import { Authenticated, Unauthenticated } from './views';
+import {Authenticated, Unauthenticated} from './views';
 import {GuardedRoute} from './components';
 
-export class App extends React.Component<any, any> {
-    public render(): JSX.Element {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/auth">
-                        <Unauthenticated/>
-                    </Route>
-                </Switch>
+export function App(): JSX.Element {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/auth">
+                    <Unauthenticated/>
+                </Route>
+            </Switch>
 
-                <Switch>
-                    <GuardedRoute component={Authenticated} exact path="/" auth={false}/>
-                </Switch>
-            </Router>
-        );
-    }
+            <Switch>
+                <GuardedRoute component={Authenticated} exact path="/" auth={false}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
