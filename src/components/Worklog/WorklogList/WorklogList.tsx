7ed330d2569cfeae8f17e353710f00f2181ea560
unknown <anzamarska@gmail.com>
2021-10-28 19:32:11 +0200

    add possibilities to add new entries

--- src/components/Worklog/WorklogList/WorklogList.tsx
+++ src/components/Worklog/WorklogList/WorklogList.tsx
@@ -2,35 +2,48 @@ import React from "react";
 import { WorklogItem } from "./WorklogItem/WorklogItem";
 import { WorklogEntry } from "../../../models/WorklogEntry";
 import TableHead from "@mui/material/TableHead";
-import { Table, TableBody, TableRow } from '@mui/material';
+import { Table, TableBody, TableRow } from "@mui/material";
 import StyledTableCell from "@mui/material/TableCell";
 
 const WorklogList = ({ worklogItems }: { worklogItems: WorklogEntry[] }) => {
-    if (worklogItems.length === 0) {
-        return (
-            <div>
-                <p>You haven't got any items to track yet. </p>
-            </div>
-        );
-    }
-
+  if (worklogItems.length === 0) {
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <StyledTableCell></StyledTableCell>
-                    <StyledTableCell align="right"></StyledTableCell>
-                    <StyledTableCell align="right"></StyledTableCell>
-                    <StyledTableCell align="right"></StyledTableCell>
-                    <StyledTableCell align="right"></StyledTableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {worklogItems.map((trackingItem: WorklogEntry) => (
-                    <WorklogItem key={trackingItem._id} item={trackingItem} />
-                ))}
-            </TableBody>
-        </Table>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {worklogItems.map((trackingItem: WorklogEntry) => (
+            <WorklogItem key={trackingItem._id} item={trackingItem} />
+          ))}
+        </TableBody>
+      </Table>
     );
+  }
+
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <StyledTableCell></StyledTableCell>
+          <StyledTableCell align="right"></StyledTableCell>
+          <StyledTableCell align="right"></StyledTableCell>
+          <StyledTableCell align="right"></StyledTableCell>
+          <StyledTableCell align="right"></StyledTableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {worklogItems.map((trackingItem: WorklogEntry) => (
+          <WorklogItem key={trackingItem._id} item={trackingItem} />
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 export default WorklogList;
