5f6ead9a6b588c066e977a8fcbad98a2e3427a20
unknown <anzamarska@gmail.com>
2021-10-28 17:37:42 +0200

    add worklog view

--- src/components/Worklog/WorklogList/WorklogItem/WorklogItem.tsx
+++ src/components/Worklog/WorklogList/WorklogItem/WorklogItem.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import { WorklogEntry } from '../../../../models/WorklogEntry';
+import React from "react";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { gql, useMutation } from "@apollo/client";
 
-export function WorklogItem({item}: { item: WorklogEntry }): JSX.Element {
+import { WorklogEntry } from "../../../../models/WorklogEntry";
+import { FETCH_ENTRIES } from "../../../../queries/useWorklogEntries";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
+const REMOVE_ENTRY = gql`
+    mutation removeEntry($id: MongoID!) {
+        entryRemoveById(_id: $id) {
+            recordId
+        }
+    }
+`;
+
+export function WorklogItem({ item }: { item: WorklogEntry }): JSX.Element {
+    const [removeEntry] = useMutation(REMOVE_ENTRY, { refetchQueries: [FETCH_ENTRIES, "getEntriesForDate"] });
+
     return (
-        <>
-            <li>
-                {
-                    <>
-                        {/* <p>{item.tag.name}</p> */}
-                        <p>{item.startTime}</p>
-                        <button onClick={() => {
-                        }}/>
-                        <button onClick={() => {
-                        }}/>
-                    </>
-                }
-            </li>
-        </>
+        <TableRow>
+            <TableCell>{item.tag?.tagBundle?.name ? item.tag.tagBundle.name : "empty name"}</TableCell>
+            <TableCell>{item.tag?.name ? item.tag?.name : "empty name"}</TableCell>
+            <TableCell>
+                <p>start time: {item.startTime}</p>
+            </TableCell>
+            <TableCell>
+                <p>end time: {item.endTime}</p>
+            </TableCell>
+            <TableCell>
+                <IconButton aria-label="delete" size="large" onClick={() => {}}>
+                    <AddCircleIcon />
+                </IconButton>
+            </TableCell>
+            <TableCell>
+                <IconButton
+                    aria-label="delete"
+                    size="large"
+                    onClick={() => {
+                        removeEntry({ variables: { id: item._id } });
+                    }}
+                >
+                    <DeleteIcon />
+                </IconButton>
+            </TableCell>
+        </TableRow>
     );
 }
--- src/components/Worklog/WorklogList/WorklogList.tsx
+++ src/components/Worklog/WorklogList/WorklogList.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import { WorklogItem } from './WorklogItem/WorklogItem';
-import { WorklogEntry } from '../../../models/WorklogEntry';
+import React from "react";
+import { WorklogItem } from "./WorklogItem/WorklogItem";
+import { WorklogEntry } from "../../../models/WorklogEntry";
+import TableHead from "@mui/material/TableHead";
+import { TableBody, TableRow } from "@mui/material";
+import StyledTableCell from "@mui/material/TableCell";
 
-const WorklogList = ({worklogItems}: { worklogItems: WorklogEntry[] }) => {
-    return worklogItems.length > 0 ? (
-        <ul>
-            {worklogItems.map((trackingItem: WorklogEntry) => (
-                <WorklogItem key={trackingItem._id} item={trackingItem}/>
-            ))}
-        </ul>
-    ) : (
-        <div>
-            <p>You haven't got any items to track yet. </p>
-        </div>
+const WorklogList = ({ worklogItems }: { worklogItems: WorklogEntry[] }) => {
+    if (worklogItems.length === 0) {
+        return (
+            <div>
+                <p>You haven't got any items to track yet. </p>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <TableHead>
+                <TableRow>
+                    <StyledTableCell></StyledTableCell>
+                    <StyledTableCell align="right"></StyledTableCell>
+                    <StyledTableCell align="right"></StyledTableCell>
+                    <StyledTableCell align="right"></StyledTableCell>
+                    <StyledTableCell align="right"></StyledTableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <ul>
+                    {worklogItems.map((trackingItem: WorklogEntry) => (
+                        <WorklogItem key={trackingItem._id} item={trackingItem} />
+                    ))}
+                </ul>
+            </TableBody>
+        </>
     );
 };
 export default WorklogList;
